66213919d1d92b3061a87a1f444d8222ad1ede1f
TenzinDhekyong <tdhekyong@gmail.com>
2018-08-23 11:12:22 +0530

    do changes

--- app/Http/Controllers/SubmitController.php
+++ app/Http/Controllers/SubmitController.php
@@ -10,13 +10,24 @@ class SubmitController extends Controller
     {
     	return view();
     }
-    public function store()
+    public function store(Request $request)
     {
-    	$storedata->projectname = request('projectname');
-    	$storedata->participant = request('participant');
-    	$storedata->category = request('category');
-    	$storedata->photo = request('photo');
-    	$storedata->description = request('description');
+    	$this->validate($request,[
+    		'projectname' => 'required',
+    		'participant'  => 'required',
+    		'category'  => 'required',
+    		'photo'  => 'required',
+    		'description' => 'required'
+    	]);
+    $post= new Submit;
+    $post->projectname = $request->input('projectname');
+    $post->participant = $request->input('participant');
+    $post->category = $request->input('category');
+    $post->photo = $request->input('photo');
+    $post->description = $request->input('description');
+    $post->save();
+
+    return redirect('/storedata')->with('success','form submitted');
 
     }
 }
